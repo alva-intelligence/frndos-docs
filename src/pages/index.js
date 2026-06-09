f6786aaa16b5fe0f6c6a8cdb02c56003264395bd
fahrizkyputra_m <fahrizkyputr@gmail.com>
2026-06-10 00:20:59 +0700

    fix: redirect home to docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,7 @@
 import React from "react";
-import Layout from "@theme/Layout";
-import { Blocks } from "../components/Blocks";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-
-const pageData = require("../../config/homepage/index.json");
+import { Redirect } from "@docusaurus/router";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-
-  return (
-    <Layout
-      title={pageData && pageData.title ? pageData.title : siteConfig.title}
-      description={
-        pageData && pageData.description
-          ? pageData.description
-          : siteConfig.tagline
-      }
-    >
-      {pageData && pageData.blocks ? <Blocks blocks={pageData.blocks} /> : null}
-    </Layout>
-  );
+  return <Redirect to={useBaseUrl("/docs/intro")} />;
 }
